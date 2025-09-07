fe25abe950ec8c7cf21879cbe38b4cb3c17fec63
Namra0904 <namramevada@gmail.com>
2025-09-07 16:34:14 +0530

    - Updated the drive link for resume

--- Protfolio/src/components/Page.jsx
+++ Protfolio/src/components/Page.jsx
@@ -103,7 +103,7 @@ export default function Page() {
               interfaces with React for modern businesses.
             </p>
             <a
-              href="https://drive.google.com/file/d/1HhdHQZ46LArv24OsgbB1hLcfS5V8kxQd/view?usp=drive_link"
+              href="https://drive.google.com/file/d/1oTNhxwlcT9_tc_VdS5eCzVtwgorwBQ0O/view?usp=drive_link"
               target="_blank"
             >
               <Button
